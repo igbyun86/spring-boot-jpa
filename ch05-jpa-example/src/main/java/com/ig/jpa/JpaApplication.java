62e2aabfd05d62c2bab7a2dde7cddcf9dc4d36c2
big <igbyun86@gmail.com>
2020-12-18 00:46:48 +0900

    Fix 불필요한 import 제거

--- ch05-jpa-example/src/main/java/com/ig/jpa/JpaApplication.java
+++ ch05-jpa-example/src/main/java/com/ig/jpa/JpaApplication.java
@@ -1,15 +1,7 @@
 package com.ig.jpa;
 
-import com.ig.jpa.entity.Member;
-import com.ig.jpa.entity.Team;
-import com.ig.jpa.util.JpaUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import java.util.List;
 
 @SpringBootApplication
 public class JpaApplication {
